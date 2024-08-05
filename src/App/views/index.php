a6009a76fd52bceffde9c90c67c2be21c472ce12
rodlez <xavier.rodlez@gmail.com>
2024-08-05 16:50:56 +0200

    Add Bootstrap Sass and fontawesome
    Include Assets to customize the Template

--- src/App/views/index.php
+++ src/App/views/index.php
@@ -4,11 +4,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Google Fonts: Source Serif 4 -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Serif+4:ital,opsz,wght@0,8..60,200..900;1,8..60,200..900&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/fontawesome.css">
     <title><?php echo $title; ?></title>
 </head>
 
 <body>
-    <h1>HOLA <?php echo $title; ?></h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-10 offset-1 bg-primary">
+                <h1><?php echo $title; ?></h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- JS -->
+    <script src="/js/script.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
