95f273d18b71a3d20319e4545be921b64781e2a5
rodlez <xavier.rodlez@gmail.com>
2024-08-05 19:41:42 +0200

    Add Factory Design Pattern in the container-definitions

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,4 +2,17 @@
 
 declare(strict_types=1);
 
-return [];
+use Framework\{TemplateEngine};
+use App\Config\Paths;
+
+// adding TemplateEngine class to the definitions
+
+// return the associative array with the key (name of the class) and the value (the function to generate the class)
+// fn() => new TemplateEngine(Paths::VIEW) -> Factory function to create an instance of the TemplateEngine class
+// using an arrow function, the at the right of the arrow => (new TemplateEngine(Paths::VIEW) is the return value of the function, no need to type return
+// the key acts as an ID for the different dependencies.
+
+
+return [
+    TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW)
+];
