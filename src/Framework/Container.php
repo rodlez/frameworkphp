80c421318a63fbc5e92c2d4521f9a895399554cc
rodlez <xavier.rodlez@gmail.com>
2024-08-05 19:45:00 +0200

    Merge arrays in the Container to addDefinitions

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -12,6 +12,11 @@ class Container
 
     public function addDefinitions(array $newDefinitions)
     {
+        // We need to merge the 2 arrays (definitions and newDefinitions), there is 2 possible solutions
+        // First using array_merge
+        // $this->definitions = array_merge($this->definitions, $newDefinitions);
+        // using the spread operator ... it's faster than using array_merge function
+        $this->definitions = [...$this->definitions, ...$newDefinitions];
         showNice($newDefinitions);
     }
 }
