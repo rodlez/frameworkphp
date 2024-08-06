fcffee54e2ce5d6117c865dd4993a3fd639e9ead
rodlez <xavier.rodlez@gmail.com>
2024-08-06 08:42:32 +0200

    Create Middleware folder to store Middlewares
    Create TemplateDataMiddleware to add a title to our Pages

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -6,10 +6,15 @@ namespace App\Config;
 
 use Framework\App;
 
-//use App\Middleware\{TemplateDataMiddleware, ValidationExceptionMiddleware, FlashMiddleware, SessionMiddleware, CsrfTokenMiddleware, CsrfGuardMiddleware};
+use App\Middleware\{TemplateDataMiddleware};
 
 // We create a function and NOT a class because Middleware can only be registered through the App instance, we are going to accept the App instance as a parameter
 
 function registerMiddleware(App $app)
 {
+    // the order of the Middleware registration does matter, our Exception class does not access to sessions until the session has been enabled
+    // therefore the SessionMiddleware must be registered last. Middleware registered last is executed first.
+    // Important register the token before the SessionMiddleware, if not we could not store the token
+
+    $app->addMiddleware(TemplateDataMiddleware::class);
 }
---
+++ src/App/Middleware/TemplateDataMiddleware.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+// To add a title to our pages, not all Controllers customize the title.
+
+class TemplateDataMiddleware
+{
+}
