1377deda6c5acc914921b7014dbde98e366e0f34
rodlez <xavier.rodlez@gmail.com>
2024-08-06 11:45:56 +0200

    Store Validation Errors

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -21,18 +21,26 @@ class Validator
     // validation will perform a field at a time
     public function validate(array $formData, array $fields)
     {
+        // to store the errors of each field
+        $errors = [];
+
         // every field in the array contains an array of rules(for example the required rule. SEE the ValidatorService validateRegister) and fieldName as the field to the key from the associative array
-        foreach ($fields as $filedName => $rules) {
+        foreach ($fields as $fieldName => $rules) {
             // multiple rules can exists for a field, we need to loop also for each rule assign to a field
             foreach ($rules as $rule) {
                 // grab the rule instance by the alias
                 $ruleValidator = $this->rules[$rule];
-                if ($ruleValidator->validate($formData, $filedName, [])) {
+                if ($ruleValidator->validate($formData, $fieldName, [])) {
                     continue;
                 }
 
-                echo "Error";
+                $errors[$fieldName][] = $ruleValidator->getMessage($formData, $fieldName, []);
             }
+        }
+
+        // if there is errors, the errors array is not empty
+        if (count($errors)) {
+            showNice($errors);
         }
     }
 }
